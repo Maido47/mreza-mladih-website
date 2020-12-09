c9bb63134af080077cb4ffa12c8ff122e9229605
sami <sami@saracus.com>
2020-12-09 13:23:34 +0100

    Authentifizierung des Users nur auf bestimmte Seiten gestellt und nicht schon bei der Index

--- src/main/java/com/example/springboot_web_app_with_login/config/SecurityConfiguration.java
+++ src/main/java/com/example/springboot_web_app_with_login/config/SecurityConfiguration.java
@@ -41,24 +41,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(
-                "/registration**",
-                "/js/**",
-                "/css/**",
-                "/countryPages/**",
-                "/img/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout")
-                .permitAll();
+        http.
+                authorizeRequests()
+                    //Hier werden alle Seiten aufgeführt, die dem User zugänglich sind,also "permit"
+                    .antMatchers("/registration**", "/js/**", "/css/**", "/countryPages/**", "/img/**","/", "/impressum").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                        .loginPage("/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll();
     }
 
 
